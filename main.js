dd52d3c9a06b86dc50a98a90aef0b6c1b8ad2f46
Karen Parisi <karen@Karens-MacBook-Air.local>
2016-08-29 09:01:05 -0700

    complete

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+function currentTime() {
+
 var clock = new Date();
 var hours = clock.getHours();
 var newHours = hours % 12;
@@ -6,25 +8,6 @@ var secs = clock.getSeconds();
 mins = mins < 10 ? '0' + mins : mins;
 secs = secs < 10 ? '0' + secs : secs;	
 
-document.getElementById("time").innerHTML = newHours + ":" + mins + ":" + secs
-
-
-
-
-// function addZero (i) {
-// 	return (i < 10) ? "0" + i : i;
-// 	}
-	
-// function getTime () {
-// 	var time = newDate();
-// 		hours = time.getHours();
-// 		newHours = hours % 12;
-// 		mins = time.getMinutes();
-// 		secs = time.getSeconds();
-	
-// document.getElementById("time").innerHTML = newHours + ":" + mins + ":" + secs; timer = setTimeout (function(){
-// getTime() }, 500);
-// }
-// getTime();
-
-// })();
+document.getElementById("time").innerHTML = newHours + ":" + mins + ":" + secs;
+t=setTimeout("currentTime()", 500);
+}
